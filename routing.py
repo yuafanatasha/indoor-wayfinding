12c32f5b655978d809170fc7b4a9471b3263d6fe
yuafanatasha <yuafanatashaa11@gmail.com>
2026-05-08 10:48:20 +0700

    ubah struktur database pada routing.py

--- routing.py
+++ routing.py
@@ -19,7 +19,12 @@ FLOOR     = args.floor
 
 # ── LOAD DATA — tabel dinamis sesuai floor ────────────────
 try:
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASS = os.getenv("DB_PASS")
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     engine = create_engine(DATABASE_URL)
     nodes   = gpd.read_postgis(f'SELECT * FROM node_{FLOOR}',    engine, geom_col='geom')
     network = gpd.read_postgis(f'SELECT * FROM network_{FLOOR}', engine, geom_col='geom')
